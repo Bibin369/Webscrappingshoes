830d9f8d1502cf2998145e65280c1f5d835e6a15
BIBIN THOMAS <81650486+Bibin369@users.noreply.github.com>
2023-08-09 00:45:19 +0530

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+base_url = "https://www.amazon.in/s?k=shoes"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+}
+
+max_retries = 5
+retry_delay = 5  # seconds
+
+for retry in range(max_retries):
+    try:
+        response = requests.get(base_url, headers=headers)
+        response.raise_for_status()
+        break  # Exit the loop if the request is successful
+    except requests.RequestException as e:
+        print(f"Request error (Retry {retry + 1}/{max_retries}):", e)
+        if retry < max_retries - 1:
+            print(f"Retrying in {retry_delay} seconds...")
+            time.sleep(retry_delay)
+        else:
+            print("Max retries reached. Exiting.")
+            exit()
+
+soup = BeautifulSoup(response.content, "html.parser")
+shoe_cards = soup.find_all("div", class_="s-result-item")
+
+shoe_data = []
+
+for shoe_card in shoe_cards:
+    try:
+        shoe_name = shoe_card.find("span", class_="a-text-normal").text.strip()
+        shoe_price = shoe_card.find("span", class_="a-offscreen").text if shoe_card.find("span", class_="a-offscreen") else "N/A"
+        shoe_rating = shoe_card.find("span", class_="a-icon-alt").text if shoe_card.find("span", class_="a-icon-alt") else "N/A"
+        
+        shoe_data.append({
+            "Name": shoe_name,
+            "Price": shoe_price,
+            "Rating": shoe_rating
+        })
+    except Exception as e:
+        print("Error in extracting shoe data:", e)
+
+df = pd.DataFrame(shoe_data)
+df.to_csv("amazon_shoes.csv", index=False)
+
+print("Data successfully extracted and CSV saved.")
